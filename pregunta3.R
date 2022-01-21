405c21017df601ceb7831217ce8e60bf36c2c6d2
Ivan Zuñiga Quiroz <ivan.zuniga@usach.cl>
2022-01-21 20:34:19 -0300

    Pregunta N°3 terminada

---
+++ pregunta3.R
@@ -0,0 +1,36 @@
+# Pregunta N°3
+
+# El Area de salud de la Universidad de Santiago quiere evaluar la situacion actual
+# de los estudiantes respecto al retorno de clases y las consecuencias que esto
+# podria traer, por el aumento de casos COVID a nivel pais.
+
+# Para ello, el area de salud junto con otros departamentos de la universidad, fabricaron
+# 2 modelos de mascarillas (X e Y) que se les facilitara a los estudiantes para un
+# retorno a clases mas seguro.
+
+# Se quiere evaluar la usabilidad de estas mascarillas, para poder implementar lo mejor 
+# de ambas mascarillas y así crear una mascarilla definitiva.
+
+# El area de salud selecciona al azar a 49 estudiantes, quienes son asignados de manera
+# aleatoria y distribuidos en 2 grupos ("grupo X" = 25 y "grupo Y" = 24)
+
+# Cada estudiante debe evaluar 5 aspectos de la mascarilla, respecto a la usabilidad y
+# seguridad de estas.
+
+# Para evaluar los aspectos de la mascarilla, se opta por utilizar la escala Likert de
+# 7 puntos, donde 1 es "muy mala" y 7 "muy buena".
+
+
+# Variables involucradas:
+# poblacion: Estudiantes de la USACH.
+# muestra: 49 estudiantes elegidos de manera aleatoria.
+# grupo X: 25 estudiantes extraidos de manera aleatoria de la muestra.
+# grupo Y: 24 estudiantes extraidos de manera aleatoria de la muestra.
+# Prueba a utilizar: Prueba de suma de rangos de Wilcoxon.
+# Escala de medicion: la escala Likert de 7 puntos
+# Niveles:  1 es "muy mala" y 7 "muy buena".
+
+# Hipotesis, la hipotesis a utilizar con respecto al contexto anterior son las siguientes:
+# H0: No hay diferencia en la usabilidad y seguridad de ambas mascarillas (se distribuyen de igual forma)
+# H1: Si hay diferencia en la usabilidad y seguridad de ambas mascarillas (distribuciones distintas).
+
